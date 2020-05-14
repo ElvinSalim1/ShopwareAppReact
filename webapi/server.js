329a621fa7615c7aa79a8b6c75af300946cc4ce1
ElvinSalim1 <elvinsalim5@gmail.com>
2020-05-14 08:38:42 +0200

    Mongodb Fails / Testa egen Mongodb

--- webapi/server.js
+++ webapi/server.js
@@ -5,6 +5,8 @@ const mongodb = require('mongoose')
 
 const serverUri = 'http://localhost:' + port
 const mongoUri = 'mongodb+srv://elvin:elvin123@cluster0-zzuof.mongodb.net/webapi?retryWrites=true&w=majority'
+//på något sätt min mongodb krånglar, det händer vanligtvis antal produkter i cart som nan,
+// jag tror att det borde lösas om du ändrar din egen mongodb
 
 http.createServer(app).listen(port, () => console.log('Webserver:' + serverUri))
 
